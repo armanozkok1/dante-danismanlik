13bac9f24f1ca11593e1f3a7d6c47699d58f0214
Arman Ozkok <ozkokarman93@gmail.com>
2026-08-13 08:48:21 +0300

    Deploy Next.js build (source: main@59315b0)

---
+++ _next/static/chunks/app/hakkimizda/page-360ed6e79d363d9d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[384,822,862],{7853:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=7853)),_N_E=e.O()}]);
